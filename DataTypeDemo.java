1bc5292cfbd5b3d62278c1ed99599330b36f079f
ankitpatelg <107167053+ankitpatelg@users.noreply.github.com>
2022-10-14 00:30:50 -0700

    Add files via upload

---
+++ DataTypeDemo.java
@@ -0,0 +1,17 @@
+class DataTypeDemo
+{
+	
+	public static void main(String args[])
+	{
+		
+		int number1= 78;
+		int number2= 67;
+		
+		
+		int sum = number1+ number2;
+		System.out.println("The sum is :"+sum); 
+		
+		
+	}
+	
+}
---
+++ DataTypeDemo2.java
@@ -0,0 +1,26 @@
+class DataTypeDemo2
+{
+	
+	public static void main(String args[])
+	{
+		
+		String name="Shuruti Singhal";
+		char gender='F';
+		int age=30;
+		float marks= 89.6f;
+		boolean pass=true;
+		
+		
+		System.out.println("Name:"+ name);
+		System.out.println("Gender:"+ gender);
+		System.out.println("Age:"+ age);
+		System.out.println("Marks:"+ marks);
+		System.out.println("Pass:"+ pass);
+		
+		
+		
+		
+		
+	}
+	
+}
