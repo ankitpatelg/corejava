1a6ce35ec8e1fd2837e47fd11718084185731738
ankitpatelg <107167053+ankitpatelg@users.noreply.github.com>
2022-10-19 01:00:45 -0700

    Add files via upload

---
+++ 18 oct/Car1.java
@@ -0,0 +1,22 @@
+/*
+Program: To understand instanceOf operator
+@author: Ankit patel
+@Date: 18 oct 2022  
+*/
+
+//import packages 
+
+import java.util.Scanner;
+//declaring a class
+class Car1
+{
+	public static void main(String args[])
+	{
+		
+		
+	}
+		
+	
+	// end of main
+}
+// end of class 
